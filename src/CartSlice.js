0819ff7aa3415b87afb3349270c91fc82b49c92e
ucheobi <ucheobi2009@yahoo.com>
2025-07-02 22:28:32 +0200

    added reducer for cart functionality

---
+++ src/CartSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const CartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [], // Initialize items as an empty array
+  },
+  reducers: {
+    addItem: (state, action) => {
+      // Check if the item already exists in the cart
+      const existingItem = state.items.find(item => item.id === action.payload.id);
+      if (existingItem) {
+        // If it exists, update the quantity
+        existingItem.quantity ++;
+      } else {
+        // If it doesn't exist, add the new item to the cart
+        state.items.push({
+          ...action.payload,
+          quantity: 1, // Initialize quantity to 1 for new items
+        });
+      }
+    },
+    removeItem: (state, action) => {
+      // Filter out the item to be removed based on its i
+      state.items = state.items.filter(item => item.id !== action.payload.id);
+    },
+    updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      // Find the item in the cart
+      const item = state.items.find(item => item.name === name);
+      if (item) {
+        // Update the item's quantity by the specified amount
+        item.quantity += quantity;
+      }
+    },
+  },
+});
+
+export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+
+export default CartSlice.reducer;
